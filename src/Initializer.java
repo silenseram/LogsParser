6672a33c2545f5e4e835caae88832c0e9e7766fc
silenseram <49502371+silenseram@users.noreply.github.com>
2019-11-28 21:26:08 +0300

    initializer fix

--- src/Initializer.java
+++ src/Initializer.java
@@ -1,3 +1,5 @@
+import Model.FileManager;
+
 import java.io.*;
 
 public class Initializer {
@@ -7,6 +9,7 @@ public class Initializer {
         createDir("C:\\Users\\" + System.getProperty("user.name")  + "\\LogsParser\\txtlogs");
         createDir("C:\\Users\\" + System.getProperty("user.name")  + "\\LogsParser\\txtlogs\\chat");
         createDir("C:\\Users\\" + System.getProperty("user.name")  + "\\LogsParser\\txtlogs\\logs");
+        createDir("C:\\Users\\" + System.getProperty("user.name")  + "\\LogsParser\\txtlogs\\full_logs");
         createDir("C:\\Users\\" + System.getProperty("user.name")  + "\\LogsParser\\config");
 
         createConfigFiles();
@@ -21,12 +24,39 @@ public class Initializer {
     private static void createConfigFiles() throws IOException {
         File mainCfg = new File("C:\\Users\\" + System.getProperty("user.name")  + "\\LogsParser\\config\\config.properties");
         File serverCfg = new File("C:\\Users\\" + System.getProperty("user.name")  + "\\LogsParser\\config\\servers.properties");
+        File timezoneCfg = new File("C:\\Users\\" + System.getProperty("user.name")  + "\\LogsParser\\config\\timezone.properties");
         if (!mainCfg.exists()){
             mainCfg.createNewFile();
             FileWriter fw = new FileWriter(mainCfg.getAbsolutePath());
             BufferedWriter bw = new BufferedWriter(fw);
 
-            bw.write("server=EasyTech");
+            bw.write("server=EasyTech\n" +
+                    "logs_server=EasyTech\n" +
+                    "theme=dark");
+            bw.close();
+        }
+        if (!timezoneCfg.exists()){
+            timezoneCfg.createNewFile();
+            FileWriter fw = new FileWriter(timezoneCfg.getAbsolutePath());
+            BufferedWriter bw = new BufferedWriter(fw);
+
+            bw.write("EasyTech=0\n" +
+                    "TechnoRPG=1\n" +
+                    "Galaxycraft=1\n" +
+                    "Lazorcraft=1\n" +
+                    "MagicRPG=2\n" +
+                    "GregTech=2\n" +
+                    "Hitech112_2=2\n" +
+                    "Hitech112_1=2\n" +
+                    "IventServer2=3\n" +
+                    "Magiccraft=3\n" +
+                    "TechnomagicRPG=3\n" +
+                    "Hitechcraft=3\n" +
+                    "Hitechcraft2=3\n" +
+                    "Hitechcraft3=3\n" +
+                    "Islandcraft=2\n" +
+                    "Technomagic=3\n" +
+                    "Technomagic2=3");
             bw.close();
         }
         if(!serverCfg.exists()){
